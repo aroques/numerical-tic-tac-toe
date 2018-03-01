6efc1be591c58960d0b836c19bb9244d5ede266b
Alec Roques <alec.roques@gmail.com>
2018-02-28 20:47:11 -0600

    Refactor is_win() function into Board class

---
+++ Board.py
@@ -0,0 +1,62 @@
+from math import sqrt
+
+
+class Board:
+
+    def __init__(self, board):
+        """Pass in a list of numbers that represent the board.
+            The length of the board must be a perfect square.
+            Zeroes represent empty spaces."""
+        length = sqrt(len(board))
+        if not length.is_integer():
+            raise ValueError('Board length is not a perfect square!')
+        self.board = board
+        self.num_columns = length
+        self.num_rows = length
+        all_numbers = range(1, len(board) + 1)
+        self.winning_sum = int(sum(all_numbers) / length)
+        print(self.winning_sum)
+
+    @property
+    def has_winning_sum(self):
+        winning_sums = [
+            self.row_has_winning_sum,
+            self.column_has_winning_sum,
+            self.diagonal_has_winning_sum
+        ]
+        return any(winning_sums)
+
+    @property
+    def row_has_winning_sum(self):
+        for i in range(self.num_columns):
+            start_idx = i * self.num_columns
+            end_idx = start_idx + self.num_columns
+            row = self.board[start_idx:end_idx]
+            if sum(row) == self.winning_sum:
+                return True
+        return False
+
+    @property
+    def column_has_winning_sum(self):
+        for i in range(self.num_rows):
+            column = self.board[i::self.num_rows]
+            if sum(column) == self.winning_sum:
+                return True
+        return False
+
+    @property
+    def diagonal_has_winning_sum(self):
+        diagonals = []
+
+        top_left_to_bottom_right = self.board[::self.num_columns + 1]
+        diagonals.append(top_left_to_bottom_right)
+
+        first_idx_of_last_row = len(self.board) - self.num_columns
+        last_idx_of_first_row = self.num_columns - 1
+        top_right_to_bottom_left = self.board[last_idx_of_first_row:first_idx_of_last_row + 1:self.num_columns - 1]
+        diagonals.append(top_right_to_bottom_left)
+
+        for diagonal in diagonals:
+            if sum(diagonal) == self.winning_sum:
+                return True
+        return False
--- main.py
+++ main.py
@@ -1,51 +1,44 @@
+from random import choice
+from Board import Board
+
+
 def main():
     print("hello world!")
 
     # 15 is a win
+
+    # player_first = int(input('Please enter a 1 or 2 to choose to go first or second:'))
+    #
+    # if player_first == 1:
+    #     print('the player is max')
+    # else:
+    #     print('the player is min')
 
     board = [
         3, 0, 4,
         1, 5, 9,
         0, 0, 2
     ]
+    # board = [
+    #     3, 0, 4, 11,
+    #     1, 5, 9, 13,
+    #     0, 0, 2, 10,
+    #     0, 0, 2
+    # ]
 
-    is_win(board)
+    board = Board(board)
+
+    # is_win(board)
+    # stub_fn(board)
 
 
-def is_win(board):
-    print(board)
-    num_columns = 3
-    num_rows = 3
-    winning_sum = 15
-
-    # check rows
-    for i in range(num_columns):
-        start_idx = i * num_columns
-        end_idx = start_idx + num_columns
-        row = board[start_idx:end_idx]
-        if sum(row) == winning_sum:
-            return True
-
-    # check columns
-    for i in range(num_rows):
-        column = board[i::num_rows]
-        if sum(column) == winning_sum:
-            return True
-
-    # check diagonals
-    diagonals = []
-
-    top_left_to_bottom_right = board[::num_columns + 1]
-    diagonals.append(top_left_to_bottom_right)
-
-    first_idx_of_last_row = len(board) - num_columns
-    last_idx_of_first_row = num_columns - 1
-    top_right_to_bottom_left = board[last_idx_of_first_row:first_idx_of_last_row + 1:num_columns - 1]
-    diagonals.append(top_right_to_bottom_left)
-
-    for diagonal in diagonals:
-        if sum(diagonal) == 15:
-            return True
+def stub_fn(board):
+    possible_moves = []
+    for i, num in enumerate(board):
+        if num == 0:
+            possible_moves.append(i)
+    for move in possible_moves:
+        print(move)
 
 
 if __name__ == '__main__':
